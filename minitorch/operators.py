10feb14a088fbaa4e959f2f613ea251b0cbb1272
namtranase <trannam.ase@gmail.com>
2021-06-15 13:22:41 +0700

    operator

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -7,42 +7,55 @@ import math
 def mul(x, y):
     ":math:`f(x, y) = x * y`"
     # TODO: Implement for Task 0.1.
+    return x * y
     raise NotImplementedError('Need to implement for Task 0.1')
 
 
 def id(x):
     ":math:`f(x) = x`"
     # TODO: Implement for Task 0.1.
+    return x
     raise NotImplementedError('Need to implement for Task 0.1')
 
 
 def add(x, y):
     ":math:`f(x, y) = x + y`"
     # TODO: Implement for Task 0.1.
+    return x + y
     raise NotImplementedError('Need to implement for Task 0.1')
 
 
 def neg(x):
     ":math:`f(x) = -x`"
     # TODO: Implement for Task 0.1.
+    return -x
     raise NotImplementedError('Need to implement for Task 0.1')
 
 
 def lt(x, y):
     ":math:`f(x) =` 1.0 if x is less than y else 0.0"
     # TODO: Implement for Task 0.1.
+    if x < y:
+        return 1.0
+    return 0.0
     raise NotImplementedError('Need to implement for Task 0.1')
 
 
 def eq(x, y):
     ":math:`f(x) =` 1.0 if x is equal to y else 0.0"
     # TODO: Implement for Task 0.1.
+    if x == y:
+        return 1.0
+    return 0.0
     raise NotImplementedError('Need to implement for Task 0.1')
 
 
 def max(x, y):
     ":math:`f(x) =` x if x is greater than y else y"
     # TODO: Implement for Task 0.1.
+    if x > y:
+        return x
+    return y
     raise NotImplementedError('Need to implement for Task 0.1')
 
 
@@ -60,6 +73,9 @@ def sigmoid(x):
 
     """
     # TODO: Implement for Task 0.1.
+    if x > 0:
+        return 1.0 / (1.0 + math.e ** -x)
+    return math.e ** x / (1.0 + math.e ** x)
     raise NotImplementedError('Need to implement for Task 0.1')
 
 
@@ -70,12 +86,18 @@ def relu(x):
     (See `<https://en.wikipedia.org/wiki/Rectifier_(neural_networks)>`_ .)
     """
     # TODO: Implement for Task 0.1.
+    if x >= 0:
+        return x
+    return 0
     raise NotImplementedError('Need to implement for Task 0.1')
 
 
 def relu_back(x, y):
     ":math:`f(x) =` y if x is greater than 0 else 0"
     # TODO: Implement for Task 0.1.
+    if x >= 0:
+        return y
+    return 0
     raise NotImplementedError('Need to implement for Task 0.1')
 
 
@@ -125,6 +147,9 @@ def map(fn):
         function : a function that takes a list and applies `fn` to each element
     """
     # TODO: Implement for Task 0.3.
+    def map_func(ls):
+        return  [fn(x) for x in ls]
+    return map_func
     raise NotImplementedError('Need to implement for Task 0.3')
 
 
@@ -150,6 +175,10 @@ def zipWith(fn):
 
     """
     # TODO: Implement for Task 0.3.
+    def zip_func(ls1, ls2):
+        assert len(ls1) != len(ls2)
+        return [fn(x1, x2) for x1, x2 in zip(ls1, ls2)]
+    return zip_func
     raise NotImplementedError('Need to implement for Task 0.3')
 
 
@@ -159,7 +188,7 @@ def addLists(ls1, ls2):
 
 
 def reduce(fn, start):
-    r"""
+    """
     Higher-order reduce.
 
     .. image:: figs/Ops/reducelist.png
@@ -176,14 +205,23 @@ def reduce(fn, start):
 
     """
     # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+    def reduce_func(ls):
+        it = iter(ls)
+        value = start
+        for element in it:
+            value = fn(value, element)
+        return value
 
+    return reduce_func
+    raise NotImplementedError('Need to implement for Task 0.3')
 
 def sum(ls):
     """
     Sum up a list using :func:`reduce` and :func:`add`.
     """
     # TODO: Implement for Task 0.3.
+
+    return reduce(add, ls[0])(ls)
     raise NotImplementedError('Need to implement for Task 0.3')
 
 
@@ -192,4 +230,5 @@ def prod(ls):
     Product of a list using :func:`reduce` and :func:`mul`.
     """
     # TODO: Implement for Task 0.3.
+    return reduce(mul, ls[0])(ls)
     raise NotImplementedError('Need to implement for Task 0.3')
