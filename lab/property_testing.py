056b2500bb00d7c2177162f321ac8638df6b8aab
namtranase <trannam.ase@gmail.com>
2021-06-17 11:02:34 +0700

    update smt you dont know

--- lab/property_testing.py
+++ lab/property_testing.py
@@ -9,4 +9,18 @@ def test_add_basic():
     # Check that order doesn't matter
     assert add(23, 3) == add(3, 23)
 
-# 
+# Naive test
+def test_add_naive():
+    for a in range(-10000, 10000):
+        for b in range(-10000, 10000):
+            assert add(a, b) == a + b
+            assert add(a, b) == add(b, a)
+
+# Using hypothesis lib
+from hypothesis.strategies import integers
+from hypothesis import given
+@given(integers(), integers())
+def test_add(a, b):
+    # Check same as slow system add
+    assert add(a, b) == a + b
+    assert add(b, b) == add(b, a)
